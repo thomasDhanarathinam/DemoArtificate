03e10769a48b1607ca4180f0d6ab3291d95dc565
thoma <thoma@Thomas.Home>
2019-12-18 22:47:06 +0000

    DemoArtificate

---
+++ src/main/java/com/rathinam/company/demoArtificate/DemoArtificateApplication.java
@@ -0,0 +1,13 @@
+package com.rathinam.company.demoArtificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoArtificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoArtificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rathinam/company/demoArtificate/DemoArtificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rathinam.company.demoArtificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoArtificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
